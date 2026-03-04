d35a07cf793cb868ccd3a8dc88c8ed28bac8c563
victor eme <victoreme140@gmail.com>
2026-03-04 11:41:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-425198752-4e2b7",
+  "appId": "1:660903508865:web:976c217be2a6f7d31fc680",
+  "apiKey": "AIzaSyDyW_bMUFz3NCwSZlP2BjzUekxVwVpaEEc",
+  "authDomain": "studio-425198752-4e2b7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "660903508865"
+};
